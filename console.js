a036f82971cb677d19b4fcfba5555cec4bac4893
Laxya Pahuja <laxya.pahuja8@gmail.com>
2021-03-07 16:14:09 +0530

    refactor

--- console.js
+++ console.js
@@ -1,10 +1,3 @@
-let requests = window.performance.getEntriesByType("resource")
 board_info_link = ''
-requests.forEach((value, index) => {
-    if (value['name'].includes('BoardFeedResource')) {
-        board_info_link = value["name"]
-    }
-})
-board_info_link = decodeURIComponent(board_info_link)
-board_info_link = board_info_link.replace('\"page_size\":25', '\"page_size\":250')
-console.log(board_info_link)
+window.performance.getEntriesByType("resource").forEach((value) => { if (value['name'].includes('BoardFeedResource')) { board_info_link = value["name"] } })
+console.log(decodeURIComponent(board_info_link).replace('\"page_size\":25', '\"page_size\":250'))
--- main.py
+++ main.py
@@ -9,15 +9,17 @@ urls = []
 
 for pin in pins:
     try:
-        url = pin['images']['orig']['url']
-        urls.append(url)
+        urls.append(pin['images']['orig']['url'])
     except KeyError:
         break
 
 if not os.path.exists(os.getcwd().replace('\\','/')+'/boards/'+pins[0]['board']['url']):
     os.makedirs(os.getcwd().replace('\\','/')+'/boards/'+pins[0]['board']['url'])
 
+n=0
 for url in urls:
+    n=n+1
     r = requests.get(url)
     with open(os.getcwd().replace('\\','/')+'/boards/'+pins[0]['board']['url']+url.split('/')[-1], "wb") as f:
         f.write(r.content)
+        print(url.split('/')[-1] + ' downloaded. ' + '[' + str(n) + '/' + str(len(urls)) + ']')
